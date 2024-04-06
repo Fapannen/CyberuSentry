defd3baaf6e211a762704249ae92cb582a52e729
Fapannen <repakt@seznam.cz>
2024-04-06 17:44:21 +0200

    Refactored inference a bit but needs more

--- inference.py
+++ inference.py
@@ -9,12 +9,60 @@ from utils.model import restore_model
 def run_inference_video():
     pass
 
+def get_cropped_faces(detected_faces, image):
+    """# Output from fp model is a tuple (bbox, confidence)
+    # ie. ([[tl_x, tl_y, br_x, br_y], [F2_tl_x, ...]], [[0.99], [0.45]])
+    # Dont forget that image is in shape (height, width)
+
+    Args:
+        detected_faces (_type_): tuple [bboxes, confidences]
+        image (_type_): The original image from which to cut faces
+
+    Returns:
+        List of images: List of images representing the cut faces.
+                        The images are preprocessed to be properly
+                        fed into the encoder network.
+    """
+    cropped_faces = []
+    for face_bbox in detected_faces[0]:
+        print(face_bbox)
+        xmin, ymin, xmax, ymax = face_bbox
+        
+        bb_xdiff = xmax - xmin
+        bb_ydiff = ymax - ymin
+
+        new_bb_xmin = int(xmin - (bb_xdiff // 4))
+        new_bb_xmin = new_bb_xmin if new_bb_xmin >= 0 else 0
+
+        new_bb_ymin = int(ymin - (bb_ydiff // 4))
+        new_bb_ymin = new_bb_ymin if new_bb_ymin >= 0 else 0
+
+        new_bb_xmax = int(xmax + (bb_xdiff // 4))
+        new_bb_xmax = new_bb_xmax if new_bb_xmax < image.shape[1] else image.shape[1] - 1
+
+        new_bb_ymax = int(ymax + (bb_ydiff // 4))
+        new_bb_ymax = new_bb_ymax if new_bb_ymax < image.shape[1] else image.shape[0] - 1
+
+        crop = image[new_bb_ymin : new_bb_ymax, new_bb_xmin : new_bb_xmax, :]
+        
+        # Prepare for feeding into the network
+        preprocessed_crop = cv2.resize(crop, (256, 256))
+        preprocessed_crop = preprocessed_crop / 255.0
+        preprocessed_crop = torch.from_numpy(preprocessed_crop)
+        preprocessed_crop = preprocessed_crop.permute(2, 0, 1).float()
+        preprocessed_crop = preprocessed_crop.unsqueeze(0)
+
+        cropped_faces.append(preprocessed_crop)
+
+    return cropped_faces
+
 def run_inference_images(model_path, img1, img2):
     img1 = read_image(img1, convert_to_tensor=False, scale=False)
     img2 = read_image(img2, convert_to_tensor=False, scale=False)
 
     fp_model = fp.MTCNN()
 
+    # Yields a tuple (bboxes, confidences)
     img1_faces = fp_model.detect(img1)
     img2_faces = fp_model.detect(img2)
 
@@ -22,55 +70,32 @@ def run_inference_images(model_path, img1, img2):
         print("No faces detected in one of the images!")
         print("Exitting ...")
         return
-    
-    # Output from fp model is a tuple (bbox, confidence)
-    # ie. ([[tl_x, tl_y, br_x, br_y], [F2_tl_x, ...]], [[0.99], [0.45]])
-    # Dont forget that image is in shape (height, width)
-    # TODO: Rewrite before publishing!!!
-    faces_cropped = []
-    for face in (img1_faces, img2_faces):
-        for face_bbox in face[0]:
-            tl_y, tl_x, br_y, br_x = face_bbox
-            bb_height = abs(tl_x - br_x)
-            bb_width = abs(tl_y - br_y)
 
-            new_bb_tlx = int(tl_x - (bb_height // 2))
-            new_bb_tlx = new_bb_tlx if new_bb_tlx >= 0 else 0
-
-            new_bb_tly = int(tl_y - (bb_width // 2))
-            new_bb_tly = new_bb_tly if new_bb_tly >= 0 else 0
-
-            new_bb_brx = int(br_x + (bb_height // 2))
-            new_bb_brx = new_bb_brx if new_bb_brx < img1.shape[0] else img1.shape[0] - 1
-
-            new_bb_bry = int(br_y + (bb_width // 2))
-            new_bb_bry = new_bb_bry if new_bb_bry < img1.shape[1] else img1.shape[1] - 1
-
-            crop = img1[new_bb_tlx : new_bb_brx, new_bb_tly : new_bb_bry, :]
-            
-            # Prepare for feeding into the network
-            preprocessed_crop = cv2.resize(crop, (256, 256))
-            preprocessed_crop = preprocessed_crop / 255.0
-            preprocessed_crop = torch.from_numpy(preprocessed_crop)
-            preprocessed_crop = preprocessed_crop.permute(2, 0, 1).float()
-            preprocessed_crop = preprocessed_crop.unsqueeze(0)
-
-            faces_cropped.append(preprocessed_crop)
+    img1_faces_cropped = get_cropped_faces(img1_faces, img1)
+    img2_faces_cropped = get_cropped_faces(img2_faces, img2)
+    faces_cropped = img1_faces_cropped + img2_faces_cropped
 
     model = restore_model(model_path, device="cpu")
 
-    faces_mapped = [(i, faces_cropped[i], model(faces_cropped[i])) for i in range(len(faces_cropped))]
+    faces_mapped = [
+        (i, faces_cropped[i], model(faces_cropped[i])) for i in range(len(faces_cropped))
+    ]
 
     for face1 in faces_mapped:
         rest = [face for face in faces_mapped if face != face1]
-        face_idx, face_cropped, face_embed = face1
-        cv2.imwrite(f"Face_{face_idx}.jpg", cv2.cvtColor(face_cropped.squeeze(0).permute(1, 2, 0).numpy() * 255, cv2.COLOR_RGB2BGR))
+        face1_idx, face1_cropped, face1_embed = face1
+
+        face_image_numpy = face1_cropped.squeeze(0).permute(1, 2, 0).numpy() * 255
+
+        # Write the image to be able to identify the faces individually
+        cv2.imwrite(f"Face_{face1_idx}.jpg", cv2.cvtColor(face_image_numpy, cv2.COLOR_RGB2BGR))
+
         for face2 in rest:
-            print(f"The difference of face {face_idx} versus face {face2[0]} is {torch.sum(face_embed - face2[2])}")
-
-        
-
-    
+            face2_idx, _, face2_embed = face2
+            face_diff = torch.sum(face1_embed - face2_embed)
+            face_cossim = (torch.nn.CosineSimilarity()(face1_embed, face2_embed)).item()
+            print(f"The difference of face {face1_idx} versus face {face2_idx} is {face_diff}"
+                  f"and their cosine similarity is {face_cossim}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Inference script")
@@ -80,4 +105,11 @@ if __name__ == "__main__":
     parser.add_argument("-v", "--video-path", action="store", dest="video_path", default="vid/sample_video.mp4")
     args = parser.parse_args()
 
-    run_inference_images(args.model_path, args.img1, args.img2)
+    if args.video_path is None and args.img1 is None:
+        raise ValueError("Missing arguments ...")
+    
+    if args.video_path is not None:
+        run_inference_video()
+
+    if args.img1 is not None and args.img2 is not None:
+        run_inference_images(args.model_path, args.img1, args.img2)
