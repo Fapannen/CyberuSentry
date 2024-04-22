eac9353951b949ca428142395a0d50154301843a
Fapannen <repakt@seznam.cz>
2024-04-22 22:24:17 +0200

    Add eva tiny

---
+++ encoder/eva_tiny.py
@@ -0,0 +1,15 @@
+import timm
+import torch.nn as nn
+
+
+class EvaTiny(nn.Module):
+    def __init__(self, num_classes=128) -> None:
+        super().__init__()
+        self.model = timm.create_model(
+            "eva02_tiny_patch14_224.mim_in22k",
+            pretrained=False,
+            num_classes=num_classes,
+        )
+
+    def forward(self, x):
+        return self.model(x)
